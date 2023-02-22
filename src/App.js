280c5657b6ca06db2174ac9f0e2e3321e2966dbf
Lymarenko Dmytro <benimaru046@gmail.com>
2023-02-22 13:54:09 +0100

    Add sort by id

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 import Ciao from "./components/Ciao";
-import HeadingH1 from "./components/HeadingH1";
-
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        { id: 10, firstName: "Brad", lastName: "Pitt" },
+        { id: 7, firstName: "Tom", lastName: "Rot" },
+        { id: 3, firstName: "Jon", lastName: "Snow" },
+        { id: 12, firstName: "Fred", lastName: "Mercury" },
+      ],
+    };
+  }
+  sortById =()=> {
+    const {users} = this.state;
+    const newUsers = [...users];
+    newUsers.sort((a,b)=>a.id-b.id);
+    this.setState({users:newUsers})
+  }
   render() {
+    const { users } = this.state;
     return (
       <>
-      <HeadingH1 />
-      <Ciao classStyle="welcome" name="Brad"/>
-      <Ciao classStyle="heading" name="Tom"/>
-      <Ciao classStyle="welcome" name="Fred"/>
-      <Ciao classStyle="heading"/>
+      <button onClick={this.sortById}>sort by id</button>
+        {users.map((user,index) => (
+          <Ciao key={index} id={user.id} name={user.firstName} classStyle="welcome" />
+        ))}
       </>
     );
   }
