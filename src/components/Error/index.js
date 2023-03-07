5da802701a04261a7ca773e93331c2d8e089a93a
Lymarenko Dmytro <benimaru046@gmail.com>
2023-03-07 14:38:36 +0100

    Add UsersLoader

---
+++ src/components/Error/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+    render() {
+        return (
+            <div>
+                <img src='images/error.png' alt='error' style={{width:'100%'}}></img>
+            </div>
+        );
+    }
+}
+
+export default Error;
