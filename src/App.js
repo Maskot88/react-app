426a2b2f5e8989d9af6990a4c6da787210ca9e7b
Lymarenko Dmytro <benimaru046@gmail.com>
2023-03-09 13:50:08 +0100

    Add window

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
-import USerLoader from "./components/UsersLoader";
+
 
 const App = (props) => {
   return (
     <>
-      <USerLoader />
+      <button onClick={()=>window.close()}>close</button>
+      <button onClick={()=>window.open('https://www.w3schools.com/js/js_window.asp')}>open w3 school</button>
+      <button onClick={()=>window.location.assign('https://www.w3schools.com')}>assign</button>
+      <button onClick={()=>window.location.replace('https://www.google.com')}>replace</button>
+      <button onClick={()=>window.location.reload()}>reload</button>
     </>
   );
 };
